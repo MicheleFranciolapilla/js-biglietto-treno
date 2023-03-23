c92e2c2a03c7f9d7cd3a2c8fbe7bee0eddb37674
MicheleFranciolapilla <michele.franciolapilla@gmail.com>
2023-03-23 12:19:41 +0900

    Calcoli e output in console completati

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,27 +7,121 @@
 // b) 40% di sconto per over 65.
 
 // Output del prezzo con due cifre decimali
+// ****************************************************
 
+//********** Costanti globali di riferimento **********
+
+// Tariffa di riferimento: 0.21 euro X km
 const       basic_fare      = 0.21;
-const       junior_price    = 0.8; //sconto 20%
-const       adult_price     = 1;   //sconto 0%
-const       senior_price    = 0.6; //sconto 40%
-let         percent_price   = 1;
+
+// Tariffa percentuale per ragazzi: 80%, ovvero sconto 20%
+const       junior_price    = 0.8; 
+
+// Tariffa percentuale per adulti: 100%, ovvero nessuno sconto
+const       adult_price     = 1;
+
+// Tariffa percentuale per over 65: 60%, ovvero sconto 40%
+const       senior_price    = 0.6;
+
+//********** Variabili globali di riferimento **********
+
+// Coefficiente moltiplicativo che determina il prezzo finale. Può assumere uno dei tre valori delle costanti junior_price, adult_price (default) o senior_price
+let         percent_price   = adult_price;
+
+// Variabile che assumerà il valore del prezzo standard (senza sconto)
+let         regular_price   = 0;
+
+// Variabile che assumerà il valore del prezzo applicato (scontato o meno, a seconda dei casi)
 let         final_price     = 0;
 
+// Variabile che assumerà il valore dello sconto applicato (laddove presente). Sarà uguale alla differenza tra il prezzo standard (regular_price) ed il prezzo applicato (final_price)
+let         discount        = 0;
+
+// Variabile stringa che definirà il tipo di viaggio: breve, medio o lungo
+let         trip            = "";
+
+// Variabile stringa che definirà il tipo di viaggiatore: ragazzo/a, adulto/a o senior
+let         traveler        = "";
+
+// Variabile stringa che definirà il trattamento applicato: Sconto 20%, Prezzo pieno, Sconto 40%
+let         percent_str     = "";
+
+// Variabili stringa rappresentanti le variabili numeriche regular_price, final_price, discount con due cifre decimali
+let         regular_str     = "";
+let         final_str       = "";  
+let         discount_str    = "";       
+
+// Funzione che recupera i dati dell'input, calcola tutti i parametri e produce output in console.log
 function calc_price()
 {
-    const   km   = document.getElementById('distance').value;
-    const   j_18 = document.getElementById('junior').checked;
-    const   mid  = document.getElementById('adult').checked;
-    const   s_65 = document.getElementById('senior').checked;
-    console.log(km);
-    console.log(j_18, mid, s_65);
+    // ********** Costanti locali **********
 
+    // Costante associata alla percorrenza selezionata
+    const   km      = document.getElementById('distance').value;
+
+    // Costanti booleane che identificano la fascia d'età selezionata
+    const   j_18    = document.getElementById('junior').checked;
+    const   mid     = document.getElementById('adult').checked;
+    const   s_65    = document.getElementById('senior').checked;
+
+    // Inizializzazione di alcune variabili sul caso di default di viaggiatore adulto (prezzo pieno)
+    percent_price = adult_price;
+    traveler = "Adulto/a";
+    percent_str = "Prezzo pieno";
+
+    // Controlli finalizzati a determinare il tipo di viaggiatore (fascia d'età), con conseguente settaggio delle relative variabili
     if (j_18)
-    {percent_price = junior_price;}
+    // Caso di viaggiatore junior (minorenne - sconto 20%)
+    {
+        percent_price = junior_price;
+        traveler = "Ragazzo/a";
+        percent_str = "Sconto 20%";
+    }
         else if (s_65)
-        {percent_price = senior_price;}
+        // Caso di viaggiatore senior (ultra sessantacinquenne - sconto 40%)
+        {
+            percent_price = senior_price;
+            traveler = "Senior";
+            percent_str = "Sconto 40%";
+        }
 
-    console.log(percent_price);
+    // Inizializzazione della stringa identificativa del tipo di viaggio, al valore di default (viaggio breve)    
+    trip = "Viaggio breve";
+    // Controlli finalizzati a determinare il tipo di viaggio (breve, medio o lungo), con settaggio della relativa variabile
+    if (km > 50)
+    // Caso di viaggio NON breve
+    {
+        if (km > 250)
+        // Caso di viaggio lungo
+        {
+            trip = "Viaggio lungo";
+        }
+        else
+        // Caso di viaggio medio
+        {
+            trip = "Viaggio medio";
+        }
+    }
+
+    // Calcolo del prezzo standard
+    regular_price = km * basic_fare;
+    regular_str = regular_price.toFixed(2);
+
+    // Calcolo del prezzo effettivo (eventualmente scontato)
+    final_price = regular_price * percent_price;
+    final_str = final_price.toFixed(2);
+
+    // Calcolo dello sconto eventualmente applicato
+    discount = regular_price - final_price;
+    discount_str = discount.toFixed(2);
+
+    // ********** Output in console **********
+    console.log(" ");
+    console.log("** INIZIO **");
+    console.log(`${trip} - ${km} Km`);
+    console.log(`${traveler} - ${percent_str}`);
+    console.log(`Prezzo: ${regular_str} €`);
+    console.log(`Pagato: ${final_str} €`);
+    console.log(`Sconto: ${discount_str} €`);
+    console.log("*** FINE ***");
 }
